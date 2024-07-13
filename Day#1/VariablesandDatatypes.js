07e1f22a02e50eb8448f97eb7afadd3491c78c55
Mian-Addi2500 <mian.kami2500@gmail.com>
2024-07-13 17:13:48 +0500

    day one task

---
+++ Day#1/VariablesandDatatypes.js
@@ -0,0 +1,55 @@
+// Task and Activites
+
+//🔻 Activity # 1  | Variable Declarations 🔻
+
+// ✍ Task # 1
+
+//  Declare A variable using [Var] , assign it a number and log the value to the console.
+
+var number = 2500;
+
+console.log(number);
+
+// ✍ Task # 2
+
+// Declare A variable using [let] , assign it a string and log the value to the console.
+
+let my_Name = "Mian Addi";
+
+console.log(my_Name);
+
+//🔻 Activity # 2  | Constant Declarations 🔻
+
+// ✍ Task # 3
+
+// Declare A variable using [Const] , assign it a boolen value and log the value to the console.
+
+const is_logIn = true;
+
+console.log(is_logIn);
+
+//🔻 Activity # 3  | Data Types 🔻
+
+// ✍ Task # 4
+
+// create variables of diff data types (number , string , boolean , object , array) and log each variable's type using the [typeof]  operator
+
+// number
+let my_num = 12345;
+
+// string
+let my_str = "hello word";
+
+// boolean
+let is_possible = true;
+
+// object
+let my_obj = {
+  name: "Mian addi",
+  id: 1,
+};
+
+// array
+let my_arr = [5, 6, 7, 8];
+
+console.log(typeof my_num, typeof my_arr, typeof my_obj, typeof my_str);
