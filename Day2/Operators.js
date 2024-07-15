15682312fc0732d1857ff6b4da748801a20a0f78
Mian-Addi2500 <mian.kami2500@gmail.com>
2024-07-15 23:03:02 +0500

    day2 work start

---
+++ Day2/Operators.js
@@ -0,0 +1,73 @@
+// Task and Activites
+
+//🔻 Activity # 1  | Arithmetic Operations 🔻
+
+// ✍ Task # 1
+
+//Write a program to add two numbers and log the result to the console
+
+let a1 = 20;
+
+let b1 = 3;
+
+console.log(a1 + b1); // output is : 23
+
+// ✍ Task # 2
+
+//Write a program to subtract two numbers and log the result to the console
+
+let a2 = 20;
+
+let b2 = 3;
+
+console.log(a2 - b2); // output is : 17
+
+// ✍ Task # 3
+
+//Write a program to multiply two numbers and log the result to the console
+
+let a3 = 20;
+
+let b3 = 3;
+
+console.log(a3 * b3); // output is : 60
+
+// ✍ Task # 4
+
+//Write a program to divid two numbers and log the result to the console
+
+let a4 = 20;
+
+let b4 = 3;
+
+console.log(a4 / b4); // output is : 6.66666667
+
+// ✍ Task # 5
+
+//Write a program to find remainder when one number is divided by another  and log the result to the console
+
+let a5 = 20;
+
+let b5 = 3;
+
+console.log(a5 % b5); // output is : 2
+
+//🔻 Activity # 2  | Assignment Operators 🔻
+
+// ✍ Task # 6
+
+//use the [+=] operator to add a number to a variable and log the result to the console
+
+let c = 25;
+c += 6;
+
+console.log(c); // output is : 31
+
+// ✍ Task # 7
+
+// use the [-=] operator to add a number to a variable and log the result to the console
+
+let d = 30;
+
+d -= 6;
+console.log(d); /// output is : 24
