a5ca13a1eab3c58eaefe1b60b21439fc362c2906
Mian-Addi2500 <mian.kami2500@gmail.com>
2024-07-17 01:06:54 +0500

    day 2 challange done

--- Day2/Operators.js
+++ Day2/Operators.js
@@ -71,3 +71,151 @@ let d = 30;
 
 d -= 6;
 console.log(d); /// output is : 24
+
+//🔻 Activity # 3  | Comparison Operators 🔻
+
+// ✍ Task # 8
+
+// write a program to compare two number using [>] and [<] and log the result to the console
+
+let e = 5;
+let f = 6;
+
+console.log(e > f); //output is : false
+
+console.log(e < f); //output is : true
+
+// ✍ Task # 9
+
+// write a program to compare two number using [>=] and [<=] and log the result to the console
+
+let g = 5;
+
+let h = 9;
+
+let i = 5;
+
+console.log(g >= h); //output is : false
+
+console.log(g >= i); //output is : true
+
+console.log(h <= g); //output is : false
+
+console.log(g <= i); //output is : true
+
+// ✍ Task # 10
+
+// write a program to compare two number using [==] and [===] and log the result to the console
+
+let j = 5;
+
+let k = 7;
+
+let l = 5;
+
+let name1 = "adil";
+
+let name2 = "kamil";
+
+let name3 = "adil";
+
+console.log(j == k); //output is : false
+
+console.log(j == i); //output is  : true
+
+console.log(j === k); //output is : false
+
+console.log(j === name1); //output is : false
+
+console.log(j === l); //output is : true
+
+console.log(name1 === name2); //output is : false
+
+console.log(name1 === name3); //output is : true
+
+//🔻 Activity # 4  | Logical Operators 🔻
+
+// ✍ Task # 11
+
+//write a program that uses the [&&] operator to combine two conditions and log the result to the console
+
+let m = 5;
+
+let n = 8;
+
+let o = 6;
+
+console.log(m > n && n > o); //output is : false
+
+console.log(n > m && n > o); //output is : true
+
+// ✍ Task # 12
+
+//write a program that uses the [||] operator to combine two conditions and log the result to the console
+
+console.log(m > n || n > o); //output is : true
+
+console.log(m > n || o > n); //output is : false
+
+// ✍ Task # 13
+
+//write a program that uses the [!] operator to negate a condition and log the result to the console
+
+console.log(m != n); //output is : true
+
+console.log(m !== n); //output is : true
+
+console.log(n != n); //output is : false
+
+//🔻 Activity # 5  | ternary Operators 🔻
+
+// ✍ Task # 14
+
+//write a program that uses the  ternary operator [?] to check if the number is negative or positive and log the result to the console
+
+let x = 5;
+
+let y = -5;
+
+// Check if the number is negative or positive using the ternary operator
+let result1 = x >= 0 ? "positive" : "negitive"; // output is : positive
+
+// Check if the number is negative or positive using the ternary operator
+let result2 = y >= 0 ? "positive" : "negitive";
+console.log(result1);
+
+console.log(result2);
+
+/*💢💢💢💢💢💢💢 Notes 💢💢💢💢💢💢💢
+
+            🔻 Feature Request  🔻
+
+
+
+1️⃣ 💨 Arithmetic Operations Script :- Write a script that performs basic arithmetic operations (additions , substraction, multiplication, division, remainder) on two numbers and log the results.
+
+2️⃣ 💨 Comparison and Logical Operators Script :- Create a script that compares two numbers using different comparison operators and combines conditions using logical operators, logging the results
+
+3️⃣ 💨 Ternary Operator Script :- Write a script that uses the ternary operator to determine if a number is positive or negative and log the results
+
+
+
+              🔻 Achievement  🔻
+
+💡 By the end of these activities, you will:💡
+
+🔷Understand and use arithmetic operators to perform basic calcultaions
+
+🔷Use assignment operators to modify the variable values
+
+🔷Compare values using comparison operators
+
+🔷Combine conditions using logical operators
+
+🔷 Use the ternary operator for concise Conditional expressions
+
+
+
+
+
+*/
